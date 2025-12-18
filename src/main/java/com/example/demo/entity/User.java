adb1f28d6bbb89924f099e1119d1b923cca83257
bala-amypo <bala@amypo.in>
2025-12-18 09:30:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -9,6 +9,43 @@ public class User{
     @Column(unique=true)
     private String email;
     private String password;
+    public User(Long id, String email, String password, Set<String> roels, LocalDateTime createdAt) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.roels = roels;
+        this.createdAt = createdAt;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setRoels(Set<String> roels) {
+        this.roels = roels;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public Set<String> getRoels() {
+        return roels;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
     private Set<String> roels;
     private LocalDateTime createdAt;
 }
