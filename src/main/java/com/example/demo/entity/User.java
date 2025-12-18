72007cf72442dd538fbef9ae0e344de7790b13d4
bala-amypo <bala@amypo.in>
2025-12-18 08:30:22 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,5 +1,9 @@
 package com.example.demo.entity;
 
 public class User{
-
-}
+    private Long id;
+    private String name;
+    private String accreitationLevel;
+    private String country;
+    Boolean active
+    }
