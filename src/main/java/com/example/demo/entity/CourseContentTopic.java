0a83282e858f9a3509df5a06fa1a915f0e7e3dcd
bala-amypo <bala@amypo.in>
2025-12-19 04:05:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/CourseContentTopic.java
+++ src/main/java/com/example/demo/entity/CourseContentTopic.java
@@ -3,10 +3,33 @@ package.com.example.demo.entity;
 import jakarta presistence.*;
 
 public class CourseContentTopic{
+    public Long getId() {
+        return id;
+    }
+    public String getTopicName() {
+        return topicName;
+    }
+    public Double getWeightPercentage() {
+        return weightPercentage;
+    }
     @Id
     GenratedValue(Strategy=GenrationType.IDENTITY)
     private Long id;
     private String topicName;
     private Double weightPercentage;
+    public CourseContentTopic(Long id, String topicName, Double weightPercentage) {
+        this.id = id;
+        this.topicName = topicName;
+        this.weightPercentage = weightPercentage;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setTopicName(String topicName) {
+        this.topicName = topicName;
+    }
+    public void setWeightPercentage(Double weightPercentage) {
+        this.weightPercentage = weightPercentage;
+    }
 
 }
--- src/main/java/com/example/demo/entity/TransferEvalutionResult.java
+++ src/main/java/com/example/demo/entity/TransferEvalutionResult.java
@@ -9,5 +9,35 @@ public class TransferEvalutionResult{
     private Double minimumOverlapPercentage;
     private Int creditHourTolerance;
     private Boolean active;
+    public TransferEvalutionResult(Long id, Double minimumOverlapPercentage, Int creditHourTolerance, Boolean active) {
+        this.id = id;
+        this.minimumOverlapPercentage = minimumOverlapPercentage;
+        this.creditHourTolerance = creditHourTolerance;
+        this.active = active;
+    }
+    public Long getId() {
+        return id;
+    }
+    public Double getMinimumOverlapPercentage() {
+        return minimumOverlapPercentage;
+    }
+    public Int getCreditHourTolerance() {
+        return creditHourTolerance;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setMinimumOverlapPercentage(Double minimumOverlapPercentage) {
+        this.minimumOverlapPercentage = minimumOverlapPercentage;
+    }
+    public void setCreditHourTolerance(Int creditHourTolerance) {
+        this.creditHourTolerance = creditHourTolerance;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 
 }
