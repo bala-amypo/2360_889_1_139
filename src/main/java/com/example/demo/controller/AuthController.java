0c20e72cd7cd53bc6888d4063705ea78b01f74d5
bala-amypo <bala@amypo.in>
2025-12-26 19:40:48 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -4,7 +4,6 @@ import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.dto.RegisterRequest;
 import com.example.demo.service.AuthService;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,17 +17,13 @@ public class AuthController {
         this.authService = authService;
     }
 
-    
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
     }
 
-    
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
-        return ResponseEntity.ok(
-                authService.login(request.getEmail(), request.getPassword())
-        );
+        return ResponseEntity.ok(authService.login(request));
     }
 }
--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -23,4 +23,5 @@ public class AuthRequest {
     public void setPassword(String password) {
         this.password = password;
     }
+    
 }
