c7c5a18fd24cbb84c4b7b2d6d59487f626641cd5
bala-amypo <bala@amypo.in>
2025-12-27 08:39:15 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
@@ -1,29 +1,29 @@
-package com.example.demo.security;
+    package com.example.demo.security;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import java.io.IOException;
+    import jakarta.servlet.*;
+    import jakarta.servlet.http.HttpServletRequest;
+    import java.io.IOException;
 
-public class JwtAuthenticationFilter implements Filter {
+    public class JwtAuthenticationFilter implements Filter {
 
-    private final JwtTokenProvider jwtTokenProvider;
+        private final JwtTokenProvider jwtTokenProvider;
 
-    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
-
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-            throws IOException, ServletException {
-
-        HttpServletRequest request = (HttpServletRequest) req;
-        String auth = request.getHeader("Authorization");
-
-        if (auth != null && auth.startsWith("Bearer ")) {
-            String token = auth.substring(7);
-            jwtTokenProvider.validateToken(token);
+        public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+            this.jwtTokenProvider = jwtTokenProvider;
         }
 
-        chain.doFilter(req, res);
+        @Override
+        public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+                throws IOException, ServletException {
+
+            HttpServletRequest request = (HttpServletRequest) req;
+            String auth = request.getHeader("Authorization");
+
+            if (auth != null && auth.startsWith("Bearer ")) {
+                String token = auth.substring(7);
+                jwtTokenProvider.validateToken(token);
+            }
+
+            chain.doFilter(req, res);
+        }
     }
-}
